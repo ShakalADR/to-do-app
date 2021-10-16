9cd9a1046ba30c4f0061fde81154111abe3790de
Daniyar <adrshin2015@gmail.com>
2021-10-16 21:26:34 +0600

    create init page

---
+++ src/components/UI/button/Btn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const BtnStyles = require('./MyBtn.module.css')
+
+function Btn({children, ...props}) {
+    return (
+        <button {...props} className={BtnStyles.MyBtn}>
+            {children}
+        </button>
+    )
+}
+
+export default Btn
